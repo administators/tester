7f875d5f8f46ddfc98f6b1499a3aea9b2fd44d3e
administators <59310370@go.buu.ac.th>
2020-03-24 09:47:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-59310370"; ?><br>
+<?php echo "ชื่อ-สกุล นาย พงศกร เวียงดอนก่อ"; ?><br>
+<iframe width="500" height="500"
+src="https://youtu.be/H0XdKVZI2AI">
+</iframe>
+</BODY>
+</HTML>
